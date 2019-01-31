798d362fddf0f05bf609310b6759d4ea002d9ae6
barro <barro@PC-CHRIS-HOME>
2019-01-31 22:18:46 +0000

    Criação do projecto Impotos-API aula 3.1. Criando o projeto da API AlgaWorks!

---
+++ src/main/java/st/gov/financas/impostosApi/ImpostosApiApplication.java
@@ -0,0 +1,14 @@
+package st.gov.financas.impostosApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpostosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpostosApiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/st/gov/financas/impostosApi/ImpostosApiApplicationTests.java
@@ -0,0 +1,17 @@
+package st.gov.financas.impostosApi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImpostosApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
