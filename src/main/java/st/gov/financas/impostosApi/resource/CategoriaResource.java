dd11961810cb18cd81c1837413784b038748d4c5
Impostos <Impostos@IMPOSTO-041.FINANCAS.ST>
2019-02-05 17:09:31 +0000

    Comite depois da aula 3.7. Desafio: Retornar 404 caso não exista a categoria

--- src/main/java/st/gov/financas/impostosApi/resource/CategoriaResource.java
+++ src/main/java/st/gov/financas/impostosApi/resource/CategoriaResource.java
@@ -5,11 +5,22 @@
  */
 package st.gov.financas.impostosApi.resource;
 
+import com.sun.net.httpserver.Authenticator.Success;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import st.gov.financas.impostosApi.model.Categoria;
 import st.gov.financas.impostosApi.repository.CategoriaRepository;
 
@@ -20,12 +31,33 @@ import st.gov.financas.impostosApi.repository.CategoriaRepository;
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
+
     @Autowired
     private CategoriaRepository categoriaRepository;
+
     @GetMapping
-    public List<Categoria> listar(){
+    public List<Categoria> listar() {
+
         return categoriaRepository.findAll();
-        
+
     }
-    
+
+    @PostMapping
+    public void criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+        Optional<Categoria> categEncotrada = categoriaRepository.findById(codigo);
+
+        if (categEncotrada.isPresent()) {
+            return new ResponseEntity(categEncotrada.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
