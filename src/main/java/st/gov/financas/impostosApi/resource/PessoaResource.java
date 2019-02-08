2a1ad62ebf82e0121791a23674dc773725e84c0e
Impostos <Impostos@IMPOSTO-041.FINANCAS.ST>
2019-02-08 10:57:46 +0000

    Comite depois da aula 3.10. Validando valores inválidos com Bean Validation

--- src/main/java/st/gov/financas/impostosApi/resource/PessoaResource.java
+++ src/main/java/st/gov/financas/impostosApi/resource/PessoaResource.java
@@ -9,15 +9,19 @@ import java.util.List;
 import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import st.gov.financas.impostosApi.event.RecursoCriadoEvent;
 import st.gov.financas.impostosApi.model.Pessoa;
@@ -45,7 +49,7 @@ public class PessoaResource {
     }
 
     @PostMapping
-    public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+    public ResponseEntity<Pessoa> criarPessoa(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
         Pessoa pessoaSalva = pessoaRepository.save(pessoa);
         publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
         return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
@@ -60,6 +64,23 @@ public class PessoaResource {
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
+
+    //HttpStatus.NO_CONTENT codigo 204 dizendo que teve sucesso mas eu não tenho nada para retornar
+    @DeleteMapping("/{codigo}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void apagarPessoa(@PathVariable Long codigo) {
+
+        pessoaRepository.deleteById(codigo);
+    }
+
+    @PutMapping("/{codigo}")
+    public ResponseEntity<Pessoa> actualizarPessoa(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
+        Optional<Pessoa> pessoaSalva = pessoaRepository.findById(codigo);
+        BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+        pessoaRepository.save(pessoaSalva);
+        return ResponseEntity.ok(pessoaSalva);
+
     }
 
 }
