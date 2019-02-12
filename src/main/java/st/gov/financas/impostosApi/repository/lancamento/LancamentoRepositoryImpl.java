2f59d00225d3664f74f40b7d7b8a3efdfa4e9ecc
Impostos <Impostos@IMPOSTO-041.FINANCAS.ST>
2019-02-12 11:56:58 +0000

    Comite depois da aula 5.9. Implementando a paginação de lançamentos

--- src/main/java/st/gov/financas/impostosApi/repository/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/st/gov/financas/impostosApi/repository/lancamento/LancamentoRepositoryImpl.java
@@ -5,6 +5,7 @@
  */
 package st.gov.financas.impostosApi.repository.lancamento;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -14,8 +15,12 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.util.StringUtils;
 import st.gov.financas.impostosApi.model.Lancamento;
+import st.gov.financas.impostosApi.model.Lancamento_;
 import st.gov.financas.impostosApi.repository.filter.LancamentoFilter;
 
 /**
@@ -28,7 +33,7 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
     private EntityManager manager;
 
     @Override
-    public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter) {
+    public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable) {
         CriteriaBuilder builder = manager.getCriteriaBuilder();
         CriteriaQuery<Lancamento> criteria = builder.createQuery(Lancamento.class);
         Root<Lancamento> root = criteria.from(Lancamento.class);
@@ -38,22 +43,22 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
         criteria.where(predicates);
 
         TypedQuery<Lancamento> query = manager.createQuery(criteria);
-
-        return null;
+        adicionarRestricoesDePaginacao(query, pageable);
+        return new PageImpl<>(query.getResultList(), pageable, total(lancamentoFilter));
     }
 
     private Predicate[] CriarRestricoes(LancamentoFilter lancamentoFilter, CriteriaBuilder builder, Root<Lancamento> root) {
         List<Predicate> predicates = new ArrayList<>();
 
         if (!StringUtils.isEmpty(lancamentoFilter.getDescricao())) {
-            predicates.add(builder.like(builder.lower(root.get(Lancamento_.descricao)), "%"+lancamentoFilter.getDescricao().toLowerCase()+"%"));
+            predicates.add(builder.like(builder.lower(root.get(Lancamento_.descricao)), "%" + lancamentoFilter.getDescricao().toLowerCase() + "%"));
         }
         if (lancamentoFilter.getDataVencimentoDe() != null) {
-//            predicates.add(e);
+            predicates.add(builder.greaterThanOrEqualTo(root.get(Lancamento_.dataVencimento), lancamentoFilter.getDataVencimentoDe()));
 
         }
         if (lancamentoFilter.getDataVencimentoAte() != null) {
-//            predicates.add(e);
+            predicates.add(builder.lessThanOrEqualTo(root.get(Lancamento_.dataVencimento), lancamentoFilter.getDataVencimentoAte()));
 
         }
 
@@ -61,4 +66,22 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 
     }
 
+    private void adicionarRestricoesDePaginacao(TypedQuery<Lancamento> query, Pageable pageable) {
+        int paginaAtual = pageable.getPageNumber();
+        int totalRegistroPorPagina = pageable.getPageSize();
+        int primeiroRegistroDaPagina = paginaAtual * totalRegistroPorPagina;
+        query.setFirstResult(primeiroRegistroDaPagina);
+        query.setMaxResults(totalRegistroPorPagina);
+    }
+
+    private Long total(LancamentoFilter lancamentoFilter) {
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
+        Root<Lancamento> root = criteria.from(Lancamento.class);
+
+        Predicate[] predicates = CriarRestricoes(lancamentoFilter, builder, root);
+        criteria.where(predicates);
+        criteria.select(builder.count(root));
+        return manager.createQuery(criteria).getSingleResult();
+    }
 }
--- src/main/java/st/gov/financas/impostosApi/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/st/gov/financas/impostosApi/repository/lancamento/LancamentoRepositoryQuery.java
@@ -6,6 +6,8 @@
 package st.gov.financas.impostosApi.repository.lancamento;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import st.gov.financas.impostosApi.model.Lancamento;
 import st.gov.financas.impostosApi.repository.filter.LancamentoFilter;
 
@@ -15,6 +17,6 @@ import st.gov.financas.impostosApi.repository.filter.LancamentoFilter;
  */
 public interface LancamentoRepositoryQuery {
     
-    public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+    public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
     
 }
