16affceb52c003aaf2ec78641eb6cc35b4d36bc0
Impostos <Impostos@IMPOSTO-041.FINANCAS.ST>
2019-02-15 16:31:49 +0000

    Comite depois da aula 5.9. Implementando a paginação de lançamentos

---
+++ src/main/java/st/gov/financas/impostosApi/token/RefreshTokenPostProcessor.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package st.gov.financas.impostosApi.token;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.StreamingHttpOutputMessage;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ *
+ * @author Impostos
+ */
+// Interface responsavel pela interceptação da requisção antes de ser escrita de volta
+@ControllerAdvice
+public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2AccessToken> {
+    
+    @Override
+    public boolean supports(MethodParameter mp, Class<? extends HttpMessageConverter<?>> type) {
+        return mp.getMethod().getName().equals("postAccessToken"); //Permite que seja intercetado apenas 
+        //quando estivesmos a trabalhar com o post
+    }
+    
+    @Override
+    public OAuth2AccessToken beforeBodyWrite(OAuth2AccessToken body, MethodParameter mp,
+            MediaType mt, Class<? extends HttpMessageConverter<?>> type,
+            ServerHttpRequest shr, ServerHttpResponse shr1) {
+        
+        HttpServletRequest req = ((ServletServerHttpRequest) shr).getServletRequest();
+        HttpServletResponse resp = ((ServletServerHttpResponse) shr1).getServletResponse();
+        String refreshToken = body.getRefreshToken().getValue();
+        
+        DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) body;
+        
+        adicionarRefreshTokenNoCookie(refreshToken, req, resp);
+        removerRefreshTokenDoBody(token);//Remover o token para não aparecer no cordo da requisição
+        return body;
+    }
+    
+    private void adicionarRefreshTokenNoCookie(String refreshToken, HttpServletRequest req, HttpServletResponse resp) {
+        Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
+        refreshTokenCookie.setHttpOnly(true);
+        refreshTokenCookie.setSecure(false);//TODO: mudar para true em producao
+        refreshTokenCookie.setPath(req.getContextPath() + "oauth/token");
+        refreshTokenCookie.setMaxAge(2592000);
+        resp.addCookie(refreshTokenCookie);
+    }
+    
+    private void removerRefreshTokenDoBody(DefaultOAuth2AccessToken token) {
+        token.setRefreshToken(null);
+    }
+    
+}
